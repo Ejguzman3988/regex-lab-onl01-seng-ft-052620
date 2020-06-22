a0cd0ba6719f542095d1e9bf522085164259cd3c
ejguzman3988 <eriberto176@gmail.com>
2020-06-22 07:53:14 -0400

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,34 @@
+
+require 'pry'
+
 def starts_with_a_vowel?(word)
+
+    !word.match(/\b[aeiouAEIOU]/).nil?
 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/un\w*ing/)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b(\S...\S)\b/).flatten
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    text.scan(/^[A-Z][\s\S]*?[!?.]/).nil?
 end
 
 def valid_phone_number?(phone)
+    text.scan[0-9]
+end
+ 
 
+def first_word_capitalized_and_ends_with_punctuation?(text)
+    text.match?(/^[A-Z].*\W$/)
+end
+
+def valid_phone_number?(number)
+    number.match?(/[0-9^\D]{10}/)
+    
 end
